7ee8090f0f94cdf4ab53fe4cfb6004f16ddb3e3a
Brandon Jordan <brandonjordan124@gmail.com>
2026-01-06 19:15:08 -0500

    Format string.h

--- string.h
+++ string.h
@@ -6,7 +6,9 @@
 #define FIDGET_STRING_H
 
 unsigned long strlen(const char *str);
+
 int count_digits(int num);
-void itoa(int num, char *number);
+
+void iota(int n, char *str);
 
 #endif //FIDGET_STRING_H
